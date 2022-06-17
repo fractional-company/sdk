da50b6a800f4e8500d3069d2a0c505f19b6fdf11
Sebastian Coronel <scoronelhamilton@gmail.com>
2022-06-17 15:34:49 -0400

    test husky pre-commits (4)

--- src/v2/components/vault.ts
+++ src/v2/components/vault.ts
@@ -1,5 +1,7 @@
 export class Vault {
-  sayHello() {
-    return 'Hello from v2';
+  sayHello(): string {
+    if (1 === 1) {
+      return console.log('Hello from vault v2');
+    }
   }
 }
--- test/utils.test.ts
+++ test/utils.test.ts
@@ -3,17 +3,11 @@ import { get2DayChange, getPercentChange } from '../src/utils/percentage';
 
 describe('Format utils', () => {
   it('should format token name The Doge NFT', () => {
-    const name = formatTokenName(
-      '0xbaac2b4491727d78d2b78815144570b9f2fe8899',
-      'The Doge NFT'
-    );
+    const name = formatTokenName('0xbaac2b4491727d78d2b78815144570b9f2fe8899', 'The Doge NFT');
     expect(name).toEqual('The Doge NFT');
   });
   it('should format token symbol DOG', () => {
-    const symbol = formatTokenSymbol(
-      '0xbaac2b4491727d78d2b78815144570b9f2fe8899',
-      'DOG'
-    );
+    const symbol = formatTokenSymbol('0xbaac2b4491727d78d2b78815144570b9f2fe8899', 'DOG');
     expect(symbol).toEqual('DOG');
   });
 });
@@ -27,5 +21,11 @@ describe('percentage utils', () => {
   it('should get the right 2 day percentage change', () => {
     const changes = get2DayChange('80', '120', '160');
     expect(changes).toEqual([-40, -0]);
+  });
+});
+
+describe('fail', () => {
+  it('should fail', () => {
+    expect(true).toBe(false);
   });
 });
