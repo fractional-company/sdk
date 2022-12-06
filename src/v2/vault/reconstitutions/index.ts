e909f24a28af283223ade506ef213d143ec1ab8b
cvetlicnifractional <86823960+cvetlicnifractional@users.noreply.github.com>
2022-12-06 16:12:27 +0100

    reexport optimistic bid interfaces

--- src/v2/vault/reconstitutions/index.ts
+++ src/v2/vault/reconstitutions/index.ts
@@ -2,3 +2,4 @@ import { Vault } from '../core/Vault';
 import { OptimisticBidModule } from './OptimisticBid';
 
 export const OptimisticBid = OptimisticBidModule(Vault);
+export * from './OptimisticBid';
